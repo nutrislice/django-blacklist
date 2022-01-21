605832c48a8caf4f4c1acb06b1e4755ba997990a
Troy <troy@nutrislice.com>
2022-01-20 18:48:12 -0700

    More tests

--- blacklist/utils.py
+++ blacklist/utils.py
@@ -4,18 +4,6 @@ _IP_PATTERN = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
 
 
 def sanitize_ip(ip_str: str) -> str:
-    """
-    >>> sanitize_ip(None)
-    ''
-    >>> sanitize_ip('')
-    ''
-    >>> sanitize_ip('65.2.3.4')
-    '65.2.3.4'
-    >>> sanitize_ip('some junk),65.26.12.345')
-    '65.26.12.345'
-    >>> sanitize_ip('65.26.12.345,some jumk),')
-    '65.26.12.345'
-    """
     if not ip_str:
         return ''
     m = _IP_PATTERN.search(ip_str)
